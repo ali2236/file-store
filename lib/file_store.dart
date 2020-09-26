4c238e44afd3996ee68b9ad56d3d9d0a311dd303
Aligator <aligator2236@gmail.com>
2020-09-26 18:48:05 +0330

    added author

--- lib/file_store.dart
+++ lib/file_store.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-import 'package:args/args.dart';
+import 'dart:io' as dio;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
 import 'package:file_store/src/models/student.dart';
@@ -12,43 +11,36 @@ import 'package:file_store/src/views/page_edit_book.dart';
 import 'package:file_store/src/views/page_edit_student.dart';
 import 'src/views/page_index.dart';
 
-const _hostname = 'localhost';
 const _basePath = 'files';
 
 void main(List<String> args) async {
-  var parser = ArgParser()..addOption('port', abbr: 'p');
-  var result = parser.parse(args);
 
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '2236';
-  var port = int.tryParse(portStr);
+  // setup routing
+  final app = Router();
+  app.get('/', indexPage);
+  await _setupStores(app);
+  await _addStoreAPIs(app);
 
-  var app = Router();
+  // start server
+  var server = await io.serve(app.handler, 'localhost', 2236);
+  print('Serving at http://${server.address.host}:${server.port}');
+  print('File Store made by Ali Ghanbari');
+  _launchBrowser(server);
+}
 
-  ///
-  /// setup stores
-  ///
-  await setupJsonStore<Student>(
-      _basePath, students, (j) => Student.fromJson(j));
+void _launchBrowser(dio.HttpServer server) => dio.Process.runSync(
+    'start http://${server.address.host}:${server.port}', [],
+    runInShell: true);
 
+Future<void> _setupStores(Router app) async{
+  await setupJsonStore<Student>(_basePath, students, (j) => Student.fromJson(j));
   await setupJsonStore<Book>(_basePath, books, (j) => Book.fromJson(j));
+}
 
-  ///
-  /// add store interaction APIs
-  ///
+Future<void> _addStoreAPIs(Router app) async{
   addStoreObjectRoutes<Student>(app, students, getStore<Student>(),
-      (uri) => Student.fromUri(uri), studentEditPage, studentsCard);
+          (uri) => Student.fromUri(uri), studentEditPage, studentsCard);
 
   addStoreObjectRoutes<Book>(app, books, getStore<Book>(),
-      (uri) => Book.fromUri(uri), bookEditPage, booksCard);
-
-  ///
-  /// add index page
-  ///
-  app.get('/', indexPage);
-
-  ///
-  /// start server
-  ///
-  var server = await io.serve(app.handler, 'localhost', port);
-  print('Serving at http://${server.address.host}:${server.port}');
+          (uri) => Book.fromUri(uri), bookEditPage, booksCard);
 }
